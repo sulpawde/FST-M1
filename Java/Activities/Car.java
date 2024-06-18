8e0fd5a7fb16caf2fb96984359ce49530c3aa811
sulpawde <33669642+sulpawde@users.noreply.github.com>
2024-06-18 14:05:21 +0530

    Add files via upload

--- Java/Activities/Car.java
+++ Java/Activities/Car.java
@@ -1,53 +1,31 @@
 public class Car {
-    private String color;
-    private String transmission;
-    private int make;
-    private int tyres;
-    private int doors;
-
-    public Car(){
-    this.tyres = 4;
-    this.doors = 4;
+    //Class Member Variables
+    String color;
+    int make;
+    String transmission;
+    int tyres;
+    int doors;
+	
+    //Constructor
+    Car() {
+	tyres = 4;
+	doors = 4;
     }
 
-    // Constructor to initialize tyres and doors
-    public Car(String sColor, String sTransmission, int iMake) {
-        this.color = sColor;
-        this.transmission = sTransmission;
-        this.make = iMake;
-        this.tyres = 4;
-        this.doors = 4;
+    //Class Methods
+    public void displayCharacterstics(){
+	System.out.println("Color of the Car: " + color);
+	System.out.println("Make of the Car: " + make);
+	System.out.println("Transmission of the Car: " + transmission);
+	System.out.println("Number of doors on the car: " + doors);
+    System.out.println("Number of tyres on the car: " + tyres);
     }
 
-    public void setColor(String sColor) {
-        this.color = sColor;
+    public void accelerate() {
+	System.out.println("Car is moving forward.");
     }
-
-    public void setTransmission(String sTransmission) {
-        this.transmission = sTransmission;
-    }
-
-    public void setMake(int iMake) {
-        this.make = iMake;
-    }
-
-    // Method - display car characteristics
-    public void displayCharacteristics() {
-        System.out.println("Car Color : " + color);
-        System.out.println("Car Transmission: " + transmission);
-        System.out.println("Car Make : " + make);
-        System.out.println("Car Tyres : " + tyres);
-        System.out.println("Car Doors : " + doors);
-    }
-
-    // Method accelarate
-    public void accelarate() {
-        System.out.println("Car is moving forward.");
-    }
-
-    // Method brake
+	
     public void brake() {
-        System.out.println("Car has stopped.");
+	System.out.println("Car has stopped.");
     }
-
 }
